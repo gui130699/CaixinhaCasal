4b89b6d41f3c2c76ed08d1b5073bec3ab5961d54
Guilherme <gui130699@users.noreply.github.com>
2026-04-06 16:40:47 -0300

    fix: corrigir deploy GitHub Pages (base URL, HashRouter, workflow CI/CD)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import { ToastProvider } from './components/ui/toast'
@@ -17,12 +17,12 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <ToastProvider>
           <App />
         </ToastProvider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
